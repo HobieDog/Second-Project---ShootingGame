e64aa914df85f72266ec71dc9e692d88bd9f0a06
HobieDog <57800848+HobieDog@users.noreply.github.com>
2019-11-29 01:43:03 +0900

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    //Spawn Delay Setting
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+
+    //GameManager UI Setting
+    public Text scoreText;
+    public Image[] lifeImg;
+    public GameObject gameOverSet;
+
+    void Update()
+    {
+        //Spawn Delay
+        curSpawnDelay += Time.deltaTime;
+
+        if(curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            //Random Spawn Time
+            maxSpawnDelay = Random.Range(0.5f, 2.5f);
+            curSpawnDelay = 0;
+        }
+
+        //UI Score Update
+        Player playerLogic = player.GetComponent<Player>();
+        //{0:n0} == 999,999,999の,
+        scoreText.text = string.Format("{0:n0}", playerLogic.score);
+    }
+
+    void SpawnEnemy()
+    {
+        //Random Spawn Enemy
+        int ranEnemy = Random.Range(0, 3);
+        //Random Spawn Point
+        int ranPoint = Random.Range(0, 9);
+        //Enemy Instance
+        GameObject enemy = Instantiate(enemyObjs[ranEnemy], 
+                                       spawnPoints[ranPoint].position,
+                                       spawnPoints[ranPoint].rotation);
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        //Player Attack Ready
+        enemyLogic.player = player;
+
+        //Enemy Move Logic
+        if(ranPoint == 6 || ranPoint == 8)          //Right Spawn
+        {
+            enemy.transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed, -1);
+        }
+        else if (ranPoint == 5 || ranPoint == 7)    //Left Spawn
+        {
+            enemy.transform.Rotate(Vector3.back * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed * (-1), -1);
+        }
+        else                                        //Front Spawn
+        {
+            rigid.velocity = new Vector2(0, enemyLogic.speed * (-1));
+        }
+    }
+
+    //Player Life
+    public void UpdateLifeIcon(int life)
+    {
+        //UI Life Init Disable
+        for (int index = 0; index < 3; index++)
+        {
+            lifeImg[index].color = new Color(1, 1, 1, 0);
+        }
+
+        //UI Life Active
+        for (int index = 0; index < life; index++)
+        {
+            lifeImg[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    //Player Respawn
+    public void InputRespawnPlayer()
+    {
+        Invoke("RespawnPlayer", 2f);
+    }
+
+    public void RespawnPlayer()
+    {
+        player.transform.position = Vector3.down * 3.5f;
+        player.SetActive(true);
+
+        Player playerLogic = player.GetComponent<Player>();
+        playerLogic.isHit = false;
+    }
+
+    //Player GameOver
+    public void GameOver()
+    {
+        gameOverSet.SetActive(true);
+    }
+
+    //Game Re:Set
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Player Setting
+    public float speed;
+    public float power;
+    public bool isTriggerTop;
+    public bool isTriggerBottom;
+    public bool isTriggerRight;
+    public bool isTriggerLeft;
+
+    //Player UI Setting
+    public int score;
+    public int life;
+
+    //Bullet
+    public GameObject BulletObjA;
+    public GameObject BulletObjB;
+
+    //Bullet Setting
+    public float maxShotDelay;
+    public float curShotDelay;
+
+    public GameManager manager;
+    public bool isHit;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Launch();
+        Reload();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if ((isTriggerRight && h == 1) || (isTriggerLeft && h == -1))
+            h = 0;
+
+        float v = Input.GetAxisRaw("Vertical");
+        if ((isTriggerTop && v == 1) || (isTriggerBottom && v == -1))
+            v = 0;
+
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+
+        //Move Animation
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+        {
+            anim.SetInteger("Input", (int)h);
+        }
+    }
+
+    void Launch()
+    { 
+        if (!Input.GetButton("Jump"))
+            return;
+
+        //Reload Delay Time
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        //Power Lever Bullet Style
+        switch (power)
+        {
+            case 1:
+                GameObject bullet = Instantiate(BulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = Instantiate(BulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
+                GameObject bulletL = Instantiate(BulletObjA, transform.position + Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletB = Instantiate(BulletObjB, transform.position, transform.rotation);
+                Rigidbody2D rigidB = bulletB.GetComponent<Rigidbody2D>();
+                rigidB.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 4:
+                GameObject bulletBB = Instantiate(BulletObjB, transform.position + Vector3.up * 0.3f, transform.rotation);
+                GameObject bulletC = Instantiate(BulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigidBB = bulletBB.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidC = bulletC.GetComponent<Rigidbody2D>();
+                rigidBB.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 5:
+                GameObject bulletBBB = Instantiate(BulletObjB, transform.position + Vector3.up * 0.2f, transform.rotation);
+                GameObject bulletRR = Instantiate(BulletObjA, transform.position + Vector3.right * 0.25f, transform.rotation);
+                GameObject bulletLL = Instantiate(BulletObjA, transform.position + Vector3.left * 0.25f, transform.rotation);
+                Rigidbody2D rigidBBB = bulletBBB.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidBBB.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+        }
+        
+        curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTriggerTop = true;
+                    break;
+                case "Bottom":
+                    isTriggerBottom = true;
+                    break;
+                case "Right":
+                    isTriggerRight = true;
+                    break;
+                case "Left":
+                    isTriggerLeft = true;
+                    break;
+            }
+        }
+
+        else if(collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet")
+        {
+            //Player Life Update
+            if (isHit)
+            {
+                life--;
+                manager.UpdateLifeIcon(life);
+            }
+            
+
+            //GameOver Check
+            if(life == 0)
+            {
+                manager.GameOver();
+            }
+            else
+            {
+                manager.InputRespawnPlayer();
+            }
+
+            
+            gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTriggerTop = false;
+                    break;
+                case "Bottom":
+                    isTriggerBottom = false;
+                    break;
+                case "Right":
+                    isTriggerRight = false;
+                    break;
+                case "Left":
+                    isTriggerLeft = false;
+                    break;
+            }
+        }
+    }
+}
