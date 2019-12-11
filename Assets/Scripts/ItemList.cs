4f27d4973cddde585a6255f811c8b972a8466f73
HobieDog <57800848+HobieDog@users.noreply.github.com>
2019-12-12 01:17:23 +0900

    Store Upgrade

---
+++ Assets/Scripts/ItemList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemList
+{
+    public string itemName;
+    public int itemUpgradeIndex;
+    public int maxitemUpgradeIndex;
+    public int itemPrice;
+    public Sprite sprite;
+}
